560d4e9d90ea22c2d29ec4e396b4d8f46c95bdd2
xiaoboJoin <xiaobo_duan@126.com>
2014-11-21 10:29:49 +0800

    done

--- NSURLSession/NSURLSession/ViewController.m
+++ NSURLSession/NSURLSession/ViewController.m
@@ -16,6 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+   // NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://www.baidu.com"]];
+    
+     NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:[NSURL URLWithString:@"http://www.baidu.com"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        if (!error) {
+            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+            if (httpResponse.statusCode == 200) {
+                NSString *string = [[NSString alloc] initWithData:data encoding:NSStringEncodingConversionAllowLossy];
+                NSLog(@"%@",string);
+            }
+        }
+
+        
+    }];
+    
+//    NSURLSessionDataTask *dataTask = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error)
+//    {
+//        if (!error) {
+//            NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+//            if (httpResponse.statusCode == 200) {
+//                NSString *string = [[NSString alloc] initWithData:data encoding:NSStringEncodingConversionAllowLossy];
+//                NSLog(@"%@",string);
+//            }
+//        }
+//    }];
+    [dataTask resume];
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
